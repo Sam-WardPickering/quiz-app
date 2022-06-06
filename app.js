897bfa36bd720c2732c0b85c7f9efe9a8d8f00de
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-06-06 16:04:14 +1200

    More work on app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
     questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
